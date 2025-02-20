29ac8cbcae3b7e30fdff785cd7656194c87f0e64
Leah123-d <95731243+Leah123-d@users.noreply.github.com>
2025-02-20 14:13:25 -0800

    Adds delete query

--- connection.js
+++ connection.js
@@ -8,7 +8,22 @@ const con = new Client ({
     database:"postgres"
 })
 
-//con.connect().then(() => console.log("connected"));
+con.connect().then(() => console.log("connected"));
+
+// to CREATE a new animal - maybe I will break this up to their own files? 
+
+// con.connect ((function(err){
+//     if(err) throw err;
+//     console.log("Connect!");
+
+//     let sql = "INSERT INTO animals (common_name, scientific_name, lifespan, habitat, diet) VALUES ('duck', 'duckaroo', 30, 'water','herbaviore')";
+//     con.query(sql, function (err, result){
+//         if(err) throw err;
+//         console.log("1 record inserted result.body"); //double check this to see how I can get what I inserted to return in the console too 
+//     });
+// }));
+
+//to READ animals 
 
 // con.query('select * from animals',(err,res) =>{
 //     if(!err){
@@ -21,31 +36,41 @@ const con = new Client ({
 //     con.end
 // })
 
-// to CREATE a new animal - maybe I will break this up to their own files? 
-// con.connect ((function(err){
-//     if(err) throw err;
-//     console.log("Connect!");
 
-//     let sql = "INSERT INTO animals (common_name, scientific_name, lifespan, habitat, diet) VALUES ('duck', 'duckaroo', 30, 'water','herbaviore')";
-//     con.query(sql, function (err, result){
-//         if(err) throw err;
-//         console.log("1 record inserted result.body"); //double check this to see how I can get what I inserted to return in the console too 
-//     });
-// }));
 
 //to UPDATE an existing animal 
 
-con.connect((function(err) {
-    if (err) throw err; 
-    console.log("error!");
+// con.connect((function(err) {
+//     if (err) throw err; 
+//     console.log("error!");
 
-    let sql = "UPDATE animals SET scientific_name = 'Anatidae' WHERE common_name = 'duck'";
+//     let sql = "UPDATE animals SET scientific_name = 'Anatidae' WHERE common_name = 'duck'";
 
-    con.query(sql, function (err, result){
-        if (err) throw err;
-        console.log(result.affectedRows + `record(s) updated`); //figure out how to get this message more clear
-    })
-}))
+//     con.query(sql, function (err, result){
+//         if (err) throw err;
+//         console.log(result.affectedRows + `record(s) updated`); //figure out how to get this message more clear
+//     })
+// }))
+
+// 
+
+//to DELETE an existing animal
+
+
+//to READ animals 
+
+let sql = "DELETE FROM animals WHERE common_name = 'duck'";
+
+ con.query(sql, (err,res) =>{
+     if(!err){
+
+         console.log(res.rows);
+     }
+     else{
+         console.log(err.message);
+     }
+     con.end
+ });
 
 
 //module.exports = con;
