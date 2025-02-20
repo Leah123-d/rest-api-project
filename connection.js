224a8de098ad68ad7dbd7f880b679ed45d0602a9
Leah123-d <95731243+Leah123-d@users.noreply.github.com>
2025-02-20 11:45:47 -0800

    Completed connection with JS file and postgres

--- connection.js
+++ connection.js
@@ -5,8 +5,19 @@ const con = new Client ({
     user:"leahputlek",
     port:5432,
     password:"",
-    database:"animals"
+    database:"postgres"
 })
 
 con.connect().then(() => console.log("connected"));
+
+con.query('select * from animals',(err,res) =>{
+    if(!err){
+
+        console.log(res.rows);
+    }
+    else{
+        console.log(err.message);
+    }
+    con.end
+})
 //module.exports = con;
