b3c88d4191fe27d258821aec85eadc101e6a85ad
Leah123-d <95731243+Leah123-d@users.noreply.github.com>
2025-02-20 14:05:09 -0800

    Adds update query

--- connection.js
+++ connection.js
@@ -21,16 +21,31 @@ const con = new Client ({
 //     con.end
 // })
 
-// to create a new animal 
-con.connect ((function(err){
-    if(err) throw err;
-    console.log("Connect!");
+// to CREATE a new animal - maybe I will break this up to their own files? 
+// con.connect ((function(err){
+//     if(err) throw err;
+//     console.log("Connect!");
 
-    let sql = "INSERT INTO animals (common_name, scientific_name, lifespan, habitat, diet) VALUES ('duck', 'duckaroo', 30, 'water','herbaviore')";
+//     let sql = "INSERT INTO animals (common_name, scientific_name, lifespan, habitat, diet) VALUES ('duck', 'duckaroo', 30, 'water','herbaviore')";
+//     con.query(sql, function (err, result){
+//         if(err) throw err;
+//         console.log("1 record inserted result.body"); //double check this to see how I can get what I inserted to return in the console too 
+//     });
+// }));
+
+//to UPDATE an existing animal 
+
+con.connect((function(err) {
+    if (err) throw err; 
+    console.log("error!");
+
+    let sql = "UPDATE animals SET scientific_name = 'Anatidae' WHERE common_name = 'duck'";
+
     con.query(sql, function (err, result){
-        if(err) throw err;
-        console.log("1 record inserted");
-    });
-}));
+        if (err) throw err;
+        console.log(result.affectedRows + `record(s) updated`); //figure out how to get this message more clear
+    })
+}))
+
 
 //module.exports = con;
